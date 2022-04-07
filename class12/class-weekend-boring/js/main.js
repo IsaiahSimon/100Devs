6bec2f3fb17aebafe4304f6fbe86d5196f25a653
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-07 17:53:47 -0400

    feat: :sparkles: Add Conditionals
    - add conditional statements
    - log output to console
    - replace console logging with innerHTML update

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -1,10 +1,22 @@
+// a Smurf to listen for the click
 document.querySelector('#check').addEventListener('click', check)
+
 
 function check() {
 
+  // gets the value out of the input
   const day = document.querySelector('#day').value
 
   //Conditionals go here
 
-
+  if (day === 'Saturday' || day === 'Sunday'){
+    // console.log("It's the weekend!");
+    document.getElementById("placeToSee").innerHTML = "It's the weekend!";
+  }else if (day === 'Tuesday' || day === 'Thursday'){
+    // console.log("It's a class day!");
+    document.getElementById("placeToSee").innerHTML = "It's a class day!";
+  }else{
+    // console.log("It's a boring day!")
+    document.getElementById("placeToSee").innerHTML = "BORRRRRRRING!!!!";
+  }
 }
