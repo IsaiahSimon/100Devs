9d95f7e84e4e6da3cdf3eea482566010fd33bdad
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-10 21:50:04 -0400

    style: :bulb: Add problem description

---
+++ the-js-way/add-conditions/following-second.js
@@ -0,0 +1,2 @@
+// Write a program that asks for a time under the form of three information (hours, minutes, seconds).
+// The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
