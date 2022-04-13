f3e4fd9f49b8aba7c1b6e7d88bbae4c778ab47f1
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-12 21:47:28 -0400

    feat: :sparkles: Add Class 27 code

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,21 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
 
+//set botScore to zero on page load only if botScore is not there
 
+if(!localStorage.getItem('botScore')){
+  localStorage.setItem('botScore', 0)
+}
+
+//
+document.querySelector('button').addEventListener('click', addAnothaOne)
+
+function addAnothaOne(){
+
+  let botScoreVal = Number(localStorage.getItem('botScore'))
+
+  //add 1 to variable
+  botScoreVal += 1
+
+  //reset score to new value + 1
+  localStorage.setItem('botScore', botScoreVal)
+}
