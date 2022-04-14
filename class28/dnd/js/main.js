c572b8df0de2b8cebee62a5fca1cf995335a42e0
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-14 19:35:37 -0400

    feat: :sparkles: Log data from API request

--- class28/dnd/js/main.js
+++ class28/dnd/js/main.js
@@ -1,17 +1,21 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
+
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
+  //assigned the data from the smurf 'click' to binding choice
   const choice = document.querySelector('input').value
+  //plug choice into the string, then make the request
   const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+
+      //console logged the data to see what we get back...
+      console.log(data)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
