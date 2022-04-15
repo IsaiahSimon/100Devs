0dbb0f22e706579ef8ca1fe85e06a56475caa9eb
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-14 20:26:59 -0400

    feat: :sparkles: Add spell subclass feature
    - Create an li from API array of objects
    - Access subclass name keys
    - Update <ul> with subclass names
    - Reset <ul> children on next click

--- class28/dnd/js/main.js
+++ class28/dnd/js/main.js
@@ -13,7 +13,34 @@ function getFetch(){
       .then(data => {
 
       //console logged the data to see what we get back...
-      console.log(data)
+      //access the subclasses of data...
+      //hard-code to access the obj, then keys
+      //console.log(data.subclasses[0].name) //reference 1st obj, then key
+      //console.log(data.subclasses[1].name) //reference 2nd obj, then key
+
+      //Loop through subclasses, and place names (keys) into console...
+      //data.subclasses.forEach( subClass => console.log(subClass.name))
+
+      //LOOP
+      data.subclasses.forEach( subClass => {
+        console.log(subClass.name)
+
+        //create an <li>
+        let li = document.createElement('li')
+        //add text to <li>
+        li.textContent = subClass.name
+        //append <li> to <ul>
+        document.querySelector('ul').appendChild(li)
+
+      })
+
+      //figure out how to clear out old <li> elements (remove/replace Children)
+      document.querySelector('button').addEventListener('click', resetUl)
+
+      function resetUl(){
+        document.querySelector('ul').replaceChildren('')
+      }
+      
       })
       .catch(err => {
           console.log(`error ${err}`)
