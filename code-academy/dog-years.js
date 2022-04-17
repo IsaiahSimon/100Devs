49fb92b7b9e3c7acf667bb537f1a216b8b883a64
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-17 12:32:50 -0400

    feat: :sparkles: Add human years to dog years conversion

--- code-academy/dog-years.js
+++ code-academy/dog-years.js
@@ -6,3 +6,29 @@
 // Each year following equates to 4 dog years.
 // Before you start doing the math in your head, let a computer take care of it! With your knowledge of math operators and variables, use JavaScript to convert your human age into dog years.
 
+//age to convert from
+const myAge = 31;
+
+//calculate the first 2 years.
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+//calculate the remaining years
+laterYears = myAge - 2;
+
+//the number of dog years accounted for by your later years.
+laterYears *= 4;
+
+//check work
+//console.log(earlyYears);
+//console.log(laterYears);
+
+//calculate myAge in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+//converts myName to lowercase letters
+myName = `Isaiah`.toLowerCase();
+
+//Final output
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
