07ac13485a01f9eaa5b044aaccbbb2a8a3273ca3
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-17 12:16:34 -0400

    feat: :sparkles: Add Newton scale conversion

--- code-academy/kelvin-weather.js
+++ code-academy/kelvin-weather.js
@@ -19,3 +19,20 @@ console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
 // What’s 0 Kelvin in Fahrenheit?
 //The temperature is -460 degrees Fahrenheit.
 
+// If you’d like extra practice, try this:
+// Convert celsius to the Newton scale using the equation below
+
+//Today's forecast
+const kelvin = 273;
+
+//Today's forecast, converted to Celcius
+const celcius = kelvin - 273;
+
+//Convert Celcius to the Newton scale
+let newton = celcius * (33/100);
+
+//Rounds down decimals
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees on the Newton scale.`)
+//The temperature is 0 degrees on the Newton scale.
