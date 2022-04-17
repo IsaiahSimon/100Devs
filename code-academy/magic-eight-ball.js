083ee83d56db65c0e855fd1dddd7cdfac743b89f
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-17 15:35:57 -0400

    feat: :sparkles: Add eightBall initial code
    - Hardcode userName and userQuestion
    - Add random num generator
    - Add switch statement logic
    - Add output

--- code-academy/magic-eight-ball.js
+++ code-academy/magic-eight-ball.js
@@ -4,3 +4,47 @@
 
 // The user will be able to input a question, then our program will output a random fortune.
 
+let userName = 'Isaiah';
+
+//greet user
+userName ? console.log(`Hello ${userName}!`) : console.log(`Hello!`);
+
+//state user's question
+let userQuestion = '"Will it rain today?"'
+console.log(`Okay ${userName}, your question was: ${userQuestion}`);
+
+//generate a random number between 0 anf 7
+let randomNumber = Math.floor(Math.random() * 8);
+
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  default:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(`Hmmm...let's see....`);
+console.log(`${eightBall}`);
