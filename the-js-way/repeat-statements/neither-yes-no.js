ab072d0e41536234239571581beca0278653ffc9
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-21 16:37:35 -0400

    feat: :sparkles: Complete tasks

--- the-js-way/repeat-statements/neither-yes-no.js
+++ the-js-way/repeat-statements/neither-yes-no.js
@@ -1,14 +1,11 @@
 // Write a program that plays "neither yes, nor no" with the user.
 //Specifically, the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
 
-let userText = prompt('Type some text:').toLowerCase();
-
-while(!(userText === 'no')||!(userText === 'yes')) {
-  userText = prompt('Type some text:').toLowerCase()
-}
-
-let num = Number(prompt('num:'))
-
-while( (num<5)||(num > 10)){
-  num = Number(prompt('num:'));
-}``
+do {
+  let userText = prompt('Type some text:').toLowerCase();
+  if((userText === 'yes')||(userText === 'no')){
+    break;
+  }else{
+    userText = prompt('Type some text:').toLowerCase();
+  };
+}while(userText !== undefined);
