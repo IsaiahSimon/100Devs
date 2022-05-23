cfac79e39ab9c8b1cb874b57a0b083b9ab7e9875
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-05-22 21:07:56 -0400

    👌 IMPROVE: Refactor
    - Refactor readFile into readWrite fn
    - Refactor else if branch logic to switch statement

--- class35/node-backend-simple-json/server.js
+++ class35/node-backend-simple-json/server.js
@@ -1,68 +1,68 @@
 const http = require('http');
-const fs = require('fs') 
+const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { // must create server 1st
+
+  // make new DRY function
+  const readWrite = (file, contentType) => {
+    fs.readFile(file, function(err, data) {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(data);
+      res.end();
+    });
+  }
+
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
+
+  switch (page) {
+    case '/':
+      readWrite('index.html', 'text/html') // DRY out
+      break;
+
+    case '/otherpage':
+      readWrite('otherpage.html', 'text/html') // DRY out
+      break;
+
+    case '/otherotherpage':
+      readWrite('otherother.html', 'text/html') // DRY out
+      break;
+
+    case '/api':
+      let personName = 'unknown';
+      let personStatus = 'unknown';
+      let personOccupation = 'unknown';
+
       if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
+        personName = 'leon';
+        personStatus = 'Boss Man';
+        personOccupation = 'Baller';
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
         const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
+          name: personName,
+          status: personStatus,
+          currentOccupation: personOccupation
         }
         res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
+        break;
+      case '/css/style.css':
+        fs.readFile('css/style.css', function(err, data) {
+          res.write(data);
+          res.end();
+        });
+        break;
+
+      case '/js/main.js':
+        readWrite('js/main.js', 'text/javascript') // DRY out
+        break;
+
+
+    default:
     figlet('404!!', function(err, data) {
       if (err) {
           console.log('Something went wrong...');
@@ -73,6 +73,7 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   }
+
 });
 
 server.listen(8000);
